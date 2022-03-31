7026f1b3f872a0dac8d673f8baace76a831c8845
Rehket <aalbright425@gmail.com>
2022-03-30 23:14:51 -0400

    Add env example script

---
+++ example_env.sh
@@ -0,0 +1,9 @@
+export PYTHONUNBUFFERED=1
+export PYTHONPATH="${BASH_SOURCE[0]}"  # Used to tell python where stuff is.
+export SERVER_NAME=localhost
+export SERVER_HOST=localhost
+export BACKEND_CORS_ORIGINS="http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8000"
+export PROJECT_NAME=FastAPI Testing
+export SECRET_KEY=')u!_Secret_So_Secret_(a1(\=%5c7ga(ou@_b_'
+export FIRST_SUPERUSER='test_user@test.com'
+export FIRST_SUPERUSER_PASSWORD=44d339666_test
